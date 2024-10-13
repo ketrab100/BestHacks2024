ac2ef8ce3f5d5f55ae5fe2bbc02a2a219be82962
Bartłomiej Sawicki <bartis300@gmail.com>
2024-10-13 10:21:04 +0200

    Matches

--- Frontend/best-hacks/src/Api/Match.ts
+++ Frontend/best-hacks/src/Api/Match.ts
@@ -1,21 +1,21 @@
-import { Match } from "@testing-library/react";
 import axios from "axios";
+import {Match} from "../Models/Interfaces";
 
-export async function getMyData() : Promise<Match[]> {
+export async function getMyData(): Promise<Match[]> {
     try {
 
         const token = localStorage.getItem('token'); // Pobierz token z localStorage
         if (!token) {
             throw new Error("No token found, please log in.");
         }
-        const response = await axios.post<Match[]>("http://localhost:2137/api/matches/me",
-        {
-            headers: {
-                'Authorization': `Bearer ${token}` // Dodaj token do nagłówka
+        const response = await axios.get<Match[]>("http://localhost:2137/api/matches/me",
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}` // Dodaj token do nagłówka
+                }
             }
-        }
-    );
-    return response.data;
+        );
+        return response.data;
     } catch (error) {
         console.error("Error:", error);
         throw error;
--- Frontend/best-hacks/src/Chat/Matches.tsx
+++ Frontend/best-hacks/src/Chat/Matches.tsx
@@ -1,10 +1,61 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './Matches.css'
-import SingleMatch, { SingleMatchProps } from "./SingleMatch";
+import SingleMatch, {SingleMatchProps} from "./SingleMatch";
+import {getMyData} from "../Api/Match";
+import {Match} from "../Models/Interfaces";
 
 export default function Matches() {
 
-    const matches: SingleMatchProps[] = [
+    const [matchess, setMatches] = useState<Match[]>([])
+
+    let matches: SingleMatchProps[] = [
+        // {
+        //     name: 'Michał',
+        //     skills: ['JS,', 'TS,', 'Angular'],
+        //     position: 'Frontend Dev Senior',
+        //     id: 'sdasd'
+        // },
+        // {
+        //     name: 'Jan',
+        //     skills: ['C++,', 'C'],
+        //     position: 'C++ Dev Senior',
+        //     id: 'sdasd'
+        // },
+        // {
+        //     name: 'Maria',
+        //     skills: ['Python,', 'Django,', 'Flask'],
+        //     position: 'Backend Dev Junior',
+        //     id: 'sdasd'
+        // },
+        // {
+        //     name: 'Franciszek',
+        //     skills: ['Talent Aquisition', 'HR'],
+        //     position: 'HR specialist regular',
+        //     id: 'sdasd'
+        // }
+    ]
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const res = await getMyData();
+
+                matches = res.map(x => {
+                    return {
+                        name: x.employer?.companyName,
+                        skills: x.employer?.tags.map((y: { name: string }) => y.name),
+                        position: x.employer?.email,
+                        id: x.employer?.id
+                    };
+                }) as SingleMatchProps[];
+            } catch (error) {
+                console.error("Failed to fetch employee data:", error);
+            }
+        };
+        getData();
+    }, []);
+
+    matches = [
         {
             name: 'Michał',
             skills: ['JS,', 'TS,', 'Angular'],
